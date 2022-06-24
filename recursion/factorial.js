7506407a85cb4517f181c432ea1e6393f9bb38af
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-06-24 20:22:15 +0530

    factorial using recursion

---
+++ recursion/factorial.js
@@ -0,0 +1,10 @@
+//Factorial
+const factorial = (n) => {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+};
+
+console.log("********** Factorial Start *********");
+console.log("Factorial ", factorial(5));
+console.log("Factorial ", factorial(10));
+console.log("********** Factorial End *********\n\n");
